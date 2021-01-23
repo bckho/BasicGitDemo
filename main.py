591cc6beb9477dd3776bc2032cefa1617a4841d4
Brion <bryan.cs@live.nl>
2021-01-23 23:10:26 +0100

    init main py file

---
+++ main.py
@@ -0,0 +1,8 @@
+
+class MyMainClass:
+    def PrintFirstLine(self):
+        message_string_first = "This is the first message!"
+        print(message_string_first)
+
+MyMainClassInstance = MyMainClass()
+MyMainClassInstance.PrintFirstLine()
