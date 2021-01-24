980b2ff9fd3bfe7865a53b8f9ab526279dcdd714
Brion <bryan.cs@live.nl>
2021-01-24 01:41:54 +0100

    Instanced alpha class in main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+from alpha import AlphaClass
 
 class MyMainClass:
     def PrintFirstLine(self):
         message_string_first = "This is the first message!"
         print(message_string_first)
 
+# Main class Instance
 MyMainClassInstance = MyMainClass()
 MyMainClassInstance.PrintFirstLine()
+
+# Alpha class instance
+AlphaClassInstance = AlphaClass()
+AlphaClassInstance.AlphaPrintLine()
